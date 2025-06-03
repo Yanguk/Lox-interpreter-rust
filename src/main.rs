df42521def12a9c478b8bedf614a5e8c9a72c298
Yanguk <yanguk@duck.com>
2025-06-03 23:23:28 +0900

    scannig: braces
    Signed-off-by: Yanguk <yanguk@duck.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
 use std::env;
 use std::fs;
 
+enum Token {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+}
+
+impl Token {
+    pub fn from_char(c: char) -> Option<Self> {
+        match c {
+            '(' => Some(Self::LeftParen),
+            ')' => Some(Self::RightParen),
+            '{' => Some(Self::LeftBrace),
+            '}' => Some(Self::RightBrace),
+            _ => None,
+        }
+    }
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::LeftParen => write!(f, "LEFT_PAREN ( null"),
+            Self::RightParen => write!(f, "RIGHT_PAREN ) null"),
+            Self::LeftBrace => write!(f, "LEFT_BRACE {{ null"),
+            Self::RightBrace => write!(f, "RIGHT_BRACE }} null"),
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -18,13 +48,19 @@ fn main() {
             });
 
             if !file_contents.is_empty() {
-                file_contents.chars().for_each(|char| {
-                    match char {
-                        '(' => println!("LEFT_PAREN {} null", char),
-                        ')' => println!("RIGHT_PAREN {} null", char),
-                        _ => panic!("Scanner not implemented"),
-                    };
-                });
+                file_contents
+                    .chars()
+                    .map(Token::from_char)
+                    .for_each(|token| {
+                        if let Some(token) = token {
+                            println!("{}", token);
+                        } else {
+                            eprintln!(
+                                "Invalid character in file: {}",
+                                token.unwrap_or(Token::LeftParen)
+                            );
+                        }
+                    });
             }
 
             println!("EOF  null");
