17128293e2891a15de5820928bf5eb5f9c0d2e92
Yanguk <yanguk@duck.com>
2025-06-21 20:16:26 +0900

    scanning: Other ...

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::env;
 use std::fs;
 
 enum Token {
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
     LeftParen,
     RightParen,
     LeftBrace,
@@ -11,6 +18,13 @@ enum Token {
 impl Token {
     pub fn from_char(c: char) -> Option<Self> {
         match c {
+            ',' => Some(Self::Comma),
+            '.' => Some(Self::Dot),
+            '-' => Some(Self::Minus),
+            '+' => Some(Self::Plus),
+            ';' => Some(Self::Semicolon),
+            '/' => Some(Self::Slash),
+            '*' => Some(Self::Star),
             '(' => Some(Self::LeftParen),
             ')' => Some(Self::RightParen),
             '{' => Some(Self::LeftBrace),
@@ -23,6 +37,13 @@ impl Token {
 impl std::fmt::Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Self::Comma => write!(f, "COMMA , null"),
+            Self::Dot => write!(f, "DOT . null"),
+            Self::Minus => write!(f, "MINUS - null"),
+            Self::Plus => write!(f, "PLUS + null"),
+            Self::Semicolon => write!(f, "SEMICOLON ; null"),
+            Self::Slash => write!(f, "SLASH / null"),
+            Self::Star => write!(f, "STAR * null"),
             Self::LeftParen => write!(f, "LEFT_PAREN ( null"),
             Self::RightParen => write!(f, "RIGHT_PAREN ) null"),
             Self::LeftBrace => write!(f, "LEFT_BRACE {{ null"),
